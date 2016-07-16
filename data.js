4eb8470d6d0b00dbfba10e6242f906b27f5dac77
Raquel Moss <raquelxmoss@gmail.com>
2016-07-16 12:28:59 +1200

    MVP 😎:

--- data.js
+++ data.js
@@ -1,174 +1,174 @@
 export default [
   {
     'event': 'Death of a spouse',
-    'units': '100'
+    'score': 100
   },
   {
     'event': 'Divorce',
-    'units': '73'
+    'score': 73
   },
   {
     'event': 'Marital separation',
-    'units': '65'
+    'score': 65
   },
   {
     'event': 'Imprisonment',
-    'units': '63'
+    'score': 63
   },
   {
     'event': 'Death of a close family member',
-    'units': '63'
+    'score': 63
   },
   {
     'event': 'Personal injury or illness',
-    'units': '53'
+    'score': 53
   },
   {
     'event': 'Marriage',
-    'units': '50'
+    'score': 50
   },
   {
     'event': 'Dismissal from work',
-    'units': '47'
+    'score': 47
   },
   {
     'event': 'Retirement',
-    'units': '45'
+    'score': 45
   },
   {
     'event': 'Marital reconciliation',
-    'units': '45'
+    'score': 45
   },
   {
     'event': 'Change in health of family member',
-    'units': '44'
+    'score': 44
   },
   {
     'event': 'Pregnancy',
-    'units': '40'
+    'score': 40
   },
   {
     'event': 'Sexual difficulties',
-    'units': '39'
+    'score': 39
   },
   {
     'event': 'Gain a new family member',
-    'units': '39'
+    'score': 39
   },
   {
     'event': 'Business readjustment',
-    'units': '39'
+    'score': 39
   },
   {
     'event': 'Change in financial state',
-    'units': '38'
+    'score': 38
   },
   {
     'event': 'Death of a close friend',
-    'units': '37'
+    'score': 37
   },
   {
     'event': 'Change to different line of work',
-    'units': '36'
+    'score': 36
   },
   {
     'event': 'Change in frequency of arguments',
-    'units': '35'
+    'score': 35
   },
   {
     'event': 'Major mortgage',
-    'units': '32'
+    'score': 32
   },
   {
     'event': 'Foreclosure of mortgage or loan',
-    'units': '30'
+    'score': 30
   },
   {
     'event': 'Trouble with in-laws',
-    'units': '29'
+    'score': 29
   },
   {
     'event': 'Child leaving home',
-    'units': '29'
+    'score': 29
   },
   {
     'event': 'Change in responsibilities at work',
-    'units': '29'
+    'score': 29
   },
   {
     'event': 'Outstanding personal achievement',
-    'units': '28'
+    'score': 28
   },
   {
     'event': 'Spouse starts or stops work',
-    'units': '26'
+    'score': 26
   },
   {
     'event': 'Beginning or end school',
-    'units': '26'
+    'score': 26
   },
   {
     'event': 'Change in living conditions',
-    'units': '25'
+    'score': 25
   },
   {
     'event': 'Revision of personal habits',
-    'units': '24'
+    'score': 24
   },
   {
     'event': 'Trouble with boss',
-    'units': '23'
+    'score': 23
   },
   {
     'event': 'Change in working hours or conditions',
-    'units': '20'
+    'score': 20
   },
   {
     'event': 'Change in schools',
-    'units': '20'
+    'score': 20
   },
   {
     'event': 'Change in residence',
-    'units': '20'
+    'score': 20
   },
   {
     'event': 'Change in recreation',
-    'units': '19'
+    'score': 19
   },
   {
     'event': 'Change in church activities',
-    'units': '19'
+    'score': 19
   },
   {
     'event': 'Change in social activities',
-    'units': '18'
+    'score': 18
   },
   {
     'event': 'Minor mortgage or loan',
-    'units': '17'
+    'score': 17
   },
   {
     'event': 'Change in sleeping habits',
-    'units': '16'
+    'score': 16
   },
   {
     'event': 'Change in number of family reunions',
-    'units': '15'
+    'score': 15
   },
   {
     'event': 'Change in eating habits',
-    'units': '15'
+    'score': 15
   },
   {
     'event': 'Vacation',
-    'units': '13'
+    'score': 13
   },
   {
     'event': 'Major Holiday',
-    'units': '12'
+    'score': 12
   },
   {
     'event': 'Minor violation of law',
-    'units': '11'
+    'score': 11
   }
 ];
--- index.js
+++ index.js
@@ -1,12 +1,84 @@
 import {run} from '@cycle/xstream-run';
-import {makeDOMDriver, div} from '@cycle/dom';
+import {makeDOMDriver, div, button} from '@cycle/dom';
 import xs from 'xstream';
 
-import data from './data';
+import questions from './data';
+
+function yesReducer (state) {
+  const question = state.questions[0];
+
+  return {
+    ...state,
+
+    score: state.score + question.score,
+    questions: state.questions.slice(1)
+  };
+}
+
+function noReducer (state) {
+  return {
+    ...state,
+
+    questions: state.questions.slice(1)
+  };
+}
+
+function renderFinalScore (score) {
+  return (
+    div('.score', `Your final score is ${score}`)
+  );
+}
+
+function view (state) {
+  if (state.questions.length === 0) {
+    return renderFinalScore(state.score);
+  }
+
+  const question = state.questions[0];
+
+  return (
+    div('.question', [
+      div('.preface', 'In the last year, have you experienced:'),
+      div('.question-text', question.event),
+      div('.buttons', [
+        button('.yes', 'Yes'),
+        button('.no', 'No')
+      ])
+    ])
+  );
+}
 
 function main ({DOM}) {
+  const yes$ = DOM
+    .select('.yes')
+    .events('click')
+    .mapTo(yesReducer);
+
+  const no$ = DOM
+    .select('.no')
+    .events('click')
+    .mapTo(noReducer);
+
+  // TODO:
+  // Given an array of stress scale questions
+  //  For each question
+  //    Render it to the DOM
+  //    Give the user the option to select YES or NO
+  //  Store the score of the user's answers
+  const initialState = {
+    questions,
+    score: 0
+  };
+
+  const reducer$ = xs.merge(
+    yes$,
+    no$
+  );
+
+  const state$ = reducer$.fold((state, reducer) => reducer(state), initialState);
+
   return {
-    DOM: xs.of(div(JSON.stringify(data)))
+    DOM: state$.map(view)
   };
 }
 
