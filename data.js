34c7730d58cd6d04c7282885ca529258df6d56b0
Raquel Moss <raquelxmoss@gmail.com>
2016-07-16 12:03:33 +1200

    Add stress scale data from Wikipedia

---
+++ data.js
@@ -0,0 +1,174 @@
+export default [
+  {
+    'event': 'Death of a spouse',
+    'units': '100'
+  },
+  {
+    'event': 'Divorce',
+    'units': '73'
+  },
+  {
+    'event': 'Marital separation',
+    'units': '65'
+  },
+  {
+    'event': 'Imprisonment',
+    'units': '63'
+  },
+  {
+    'event': 'Death of a close family member',
+    'units': '63'
+  },
+  {
+    'event': 'Personal injury or illness',
+    'units': '53'
+  },
+  {
+    'event': 'Marriage',
+    'units': '50'
+  },
+  {
+    'event': 'Dismissal from work',
+    'units': '47'
+  },
+  {
+    'event': 'Retirement',
+    'units': '45'
+  },
+  {
+    'event': 'Marital reconciliation',
+    'units': '45'
+  },
+  {
+    'event': 'Change in health of family member',
+    'units': '44'
+  },
+  {
+    'event': 'Pregnancy',
+    'units': '40'
+  },
+  {
+    'event': 'Sexual difficulties',
+    'units': '39'
+  },
+  {
+    'event': 'Gain a new family member',
+    'units': '39'
+  },
+  {
+    'event': 'Business readjustment',
+    'units': '39'
+  },
+  {
+    'event': 'Change in financial state',
+    'units': '38'
+  },
+  {
+    'event': 'Death of a close friend',
+    'units': '37'
+  },
+  {
+    'event': 'Change to different line of work',
+    'units': '36'
+  },
+  {
+    'event': 'Change in frequency of arguments',
+    'units': '35'
+  },
+  {
+    'event': 'Major mortgage',
+    'units': '32'
+  },
+  {
+    'event': 'Foreclosure of mortgage or loan',
+    'units': '30'
+  },
+  {
+    'event': 'Trouble with in-laws',
+    'units': '29'
+  },
+  {
+    'event': 'Child leaving home',
+    'units': '29'
+  },
+  {
+    'event': 'Change in responsibilities at work',
+    'units': '29'
+  },
+  {
+    'event': 'Outstanding personal achievement',
+    'units': '28'
+  },
+  {
+    'event': 'Spouse starts or stops work',
+    'units': '26'
+  },
+  {
+    'event': 'Beginning or end school',
+    'units': '26'
+  },
+  {
+    'event': 'Change in living conditions',
+    'units': '25'
+  },
+  {
+    'event': 'Revision of personal habits',
+    'units': '24'
+  },
+  {
+    'event': 'Trouble with boss',
+    'units': '23'
+  },
+  {
+    'event': 'Change in working hours or conditions',
+    'units': '20'
+  },
+  {
+    'event': 'Change in schools',
+    'units': '20'
+  },
+  {
+    'event': 'Change in residence',
+    'units': '20'
+  },
+  {
+    'event': 'Change in recreation',
+    'units': '19'
+  },
+  {
+    'event': 'Change in church activities',
+    'units': '19'
+  },
+  {
+    'event': 'Change in social activities',
+    'units': '18'
+  },
+  {
+    'event': 'Minor mortgage or loan',
+    'units': '17'
+  },
+  {
+    'event': 'Change in sleeping habits',
+    'units': '16'
+  },
+  {
+    'event': 'Change in number of family reunions',
+    'units': '15'
+  },
+  {
+    'event': 'Change in eating habits',
+    'units': '15'
+  },
+  {
+    'event': 'Vacation',
+    'units': '13'
+  },
+  {
+    'event': 'Major Holiday',
+    'units': '12'
+  },
+  {
+    'event': 'Minor violation of law',
+    'units': '11'
+  }
+];
--- index.js
+++ index.js
@@ -2,9 +2,11 @@ import {run} from '@cycle/xstream-run';
 import {makeDOMDriver, div} from '@cycle/dom';
 import xs from 'xstream';
 
+import data from './data';
+
 function main ({DOM}) {
   return {
-    DOM: xs.of(div('hello world'))
+    DOM: xs.of(div(JSON.stringify(data)))
   };
 }
 
